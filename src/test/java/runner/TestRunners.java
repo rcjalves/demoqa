8e7071ed66bba17b1bb656e942215025a4330e1a
Rian Jesus <rian.alves@deltapoint.com.br>
2025-04-10 11:25:27 -0300

    ajustes

--- src/test/java/runner/TestRunners.java
+++ src/test/java/runner/TestRunners.java
@@ -6,15 +6,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "src/test/resources/features",
-    glue = "steps",
-    tags = "@crud",
-    plugin = {"pretty", "html:target/cucumber-report.html"},
-
-    monochrome = true
+  features = "src/test/resources/features",
+  glue = {"steps", "hooks"},
+  plugin = {"pretty", "html:target/cucumber-report.html"},
+  tags = "@crud",
+  monochrome = true
 )
-
-
-public class TestRunners {
-}
-
+public class TestRunners {}
